ee76d4394d272ea5733b848a6c3a7ca0fa03bbb2
Ilya Radchenko <knownasilya@gmail.com>
2020-11-04 23:16:45 -0500

    copy

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,24 @@
-use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use specs_derive::Component;
-use std::cmp::{max, min};
-
+mod components;
+pub use components::*;
 mod map;
-pub use map::{Map, TileType};
+pub use map::*;
+mod player;
+use player::*;
+mod rect;
+pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(Component)]
-struct LeftMover {}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-struct State {
-    ecs: World,
+pub struct State {
+    pub ecs: World,
 }
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -59,8 +30,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Map>();
-        map.draw_map(ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -74,70 +44,36 @@ impl GameState for State {
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-        .with_title("moonshot")
-        .with_font("terminal8x8_palette2.png", 8, 8)
+        .with_title("Roguelike Tutorial")
         .build()?;
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    // gs.ecs.insert(new_map_test());
-    gs.ecs.insert(Map::new_map_start());
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.ecs.insert(map);
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 5, y: 5 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::BLUE),
-            bg: RGB::named(rltk::WHITE),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+            dirty: true,
+        })
         .build();
 
-    for i in 1..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::WHITE),
-            })
-            // .with(LeftMover {})
-            .build();
-    }
-
     rltk::main_loop(context, gs)
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Map>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = Map::xy_idx(map.width, pos.x + delta_x, pos.y + delta_y);
-
-        if !map.tiles[destination_idx].is_blocked() {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // Player movement
-    match ctx.key {
-        None => {} // Nothing happened
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
 }
--- src/map.rs
+++ src/map.rs
@@ -1,137 +1,153 @@
-use rltk::{Rltk, RGB};
+use super::Rect;
+use rltk::{Algorithm2D, BaseMap, Point, RandomNumberGenerator, Rltk, RGB};
+use specs::prelude::*;
+use std::cmp::{max, min};
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
   Wall,
   Floor,
-  Tree,
 }
 
-impl TileType {
-  pub fn is_blocked(&self) -> bool {
-    match *self {
-      TileType::Wall => true,
-      TileType::Tree => true,
-      _ => false,
-    }
-  }
-}
-
+#[derive(Default)]
 pub struct Map {
   pub tiles: Vec<TileType>,
+  pub rooms: Vec<Rect>,
   pub width: i32,
   pub height: i32,
+  pub revealed_tiles: Vec<bool>,
+  pub visible_tiles: Vec<bool>,
 }
 
 impl Map {
-  pub fn draw_map(&self, ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-    for tile in self.tiles.iter() {
-      // Render a tile depending upon the tile type
+  pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+    (y as usize * self.width as usize) + x as usize
+  }
+
+  fn apply_room_to_map(&mut self, room: &Rect) {
+    for y in room.y1 + 1..=room.y2 {
+      for x in room.x1 + 1..=room.x2 {
+        let idx = self.xy_idx(x, y);
+        self.tiles[idx] = TileType::Floor;
+      }
+    }
+  }
+
+  fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+      let idx = self.xy_idx(x, y);
+      if idx > 0 && idx < self.width as usize * self.height as usize {
+        self.tiles[idx as usize] = TileType::Floor;
+      }
+    }
+  }
+
+  fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+      let idx = self.xy_idx(x, y);
+      if idx > 0 && idx < self.width as usize * self.height as usize {
+        self.tiles[idx as usize] = TileType::Floor;
+      }
+    }
+  }
+
+  /// Makes a new map using the algorithm from http://rogueliketutorials.com/tutorials/tcod/part-3/
+  /// This gives a handful of random rooms and corridors joining them together.
+  pub fn new_map_rooms_and_corridors() -> Map {
+    let mut map = Map {
+      tiles: vec![TileType::Wall; 80 * 50],
+      rooms: Vec::new(),
+      width: 80,
+      height: 50,
+      revealed_tiles: vec![false; 80 * 50],
+      visible_tiles: vec![false; 80 * 50],
+    };
+
+    const MAX_ROOMS: i32 = 30;
+    const MIN_SIZE: i32 = 6;
+    const MAX_SIZE: i32 = 10;
+
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..MAX_ROOMS {
+      let w = rng.range(MIN_SIZE, MAX_SIZE);
+      let h = rng.range(MIN_SIZE, MAX_SIZE);
+      let x = rng.roll_dice(1, map.width - w - 1) - 1;
+      let y = rng.roll_dice(1, map.height - h - 1) - 1;
+      let new_room = Rect::new(x, y, w, h);
+      let mut ok = true;
+      for other_room in map.rooms.iter() {
+        if new_room.intersect(other_room) {
+          ok = false
+        }
+      }
+      if ok {
+        map.apply_room_to_map(&new_room);
+
+        if !map.rooms.is_empty() {
+          let (new_x, new_y) = new_room.center();
+          let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].center();
+          if rng.range(0, 2) == 1 {
+            map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
+            map.apply_vertical_tunnel(prev_y, new_y, new_x);
+          } else {
+            map.apply_vertical_tunnel(prev_y, new_y, prev_x);
+            map.apply_horizontal_tunnel(prev_x, new_x, new_y);
+          }
+        }
+
+        map.rooms.push(new_room);
+      }
+    }
+
+    map
+  }
+}
+
+impl BaseMap for Map {
+  fn is_opaque(&self, idx: usize) -> bool {
+    self.tiles[idx] == TileType::Wall
+  }
+}
+
+impl Algorithm2D for Map {
+  fn dimensions(&self) -> Point {
+    Point::new(self.width, self.height)
+  }
+}
+
+pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
+  let map = ecs.fetch::<Map>();
+
+  let mut y = 0;
+  let mut x = 0;
+  for (idx, tile) in map.tiles.iter().enumerate() {
+    // Render a tile depending upon the tile type
+
+    if map.revealed_tiles[idx] {
+      let glyph;
+      let mut fg;
       match tile {
         TileType::Floor => {
-          ctx.set(
-            x,
-            y,
-            RGB::from_f32(0.431, 0.475, 0.467),
-            // RGB::from_f32(0., 0., 0.),
-            // RGB::named(rltk::GREEN),
-            RGB::named(rltk::WHITE),
-            rltk::to_cp437('.'),
-          );
+          glyph = rltk::to_cp437('.');
+          fg = RGB::from_f32(0.0, 0.5, 0.5);
         }
         TileType::Wall => {
-          ctx.set(
-            x,
-            y,
-            RGB::from_f32(0.188, 0.239, 0.231),
-            // RGB::from_f32(0., 0., 0.),
-            // RGB::named(rltk::GREEN),
-            RGB::named(rltk::WHITE),
-            rltk::to_cp437('#'),
-          );
-        }
-        TileType::Tree => {
-          ctx.set(
-            x,
-            y,
-            // RGB::from_f32(0.188, 0.239, 0.231),
-            // RGB::from_f32(0., 0., 0.),
-            RGB::named(rltk::DARK_OLIVE),
-            RGB::named(rltk::WHITE),
-            rltk::to_cp437('♣'),
-          );
+          glyph = rltk::to_cp437('#');
+          fg = RGB::from_f32(0., 1.0, 0.);
         }
       }
-
-      // Move the coordinates
-      x += 1;
-      if x > 79 {
-        x = 0;
-        y += 1;
+      if !map.visible_tiles[idx] {
+        fg = fg.to_greyscale()
       }
-    }
-  }
-
-  pub fn xy_idx(width: i32, x: i32, y: i32) -> usize {
-    (y as usize * width as usize) + x as usize
-  }
-
-  pub fn new_map_start() -> Map {
-    let mut map = Map {
-      tiles: vec![TileType::Floor; 80 * 50],
-      width: 80,
-      height: 50,
-    };
-
-    // Make the boundaries walls
-    for x in 0..80 {
-      map.tiles[Map::xy_idx(map.width, x, 0)] = TileType::Wall;
-      map.tiles[Map::xy_idx(map.width, x, 49)] = TileType::Wall;
-    }
-    for y in 0..50 {
-      map.tiles[Map::xy_idx(map.width, 0, y)] = TileType::Wall;
-      map.tiles[Map::xy_idx(map.width, 79, y)] = TileType::Wall;
+      ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
     }
 
-    map.tiles[Map::xy_idx(map.width, 20, 15)] = TileType::Tree;
-
-    map
-  }
-
-  /// Makes a map with solid boundaries and 400 randomly placed walls. No guarantees that it won't
-  /// look awful.
-  pub fn new_map_test() -> Map {
-    let mut map = Map {
-      tiles: vec![TileType::Floor; 80 * 50],
-      width: 80,
-      height: 50,
-    };
-
-    // Make the boundaries walls
-    for x in 0..80 {
-      map.tiles[Map::xy_idx(map.width, x, 0)] = TileType::Wall;
-      map.tiles[Map::xy_idx(map.width, x, 49)] = TileType::Wall;
+    // Move the coordinates
+    x += 1;
+    if x > 79 {
+      x = 0;
+      y += 1;
     }
-    for y in 0..50 {
-      map.tiles[Map::xy_idx(map.width, 0, y)] = TileType::Wall;
-      map.tiles[Map::xy_idx(map.width, 79, y)] = TileType::Wall;
-    }
-
-    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
-    // First, obtain the thread-local RNG:
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-      let x = rng.roll_dice(1, 79);
-      let y = rng.roll_dice(1, 49);
-      let idx = Map::xy_idx(map.width, x, y);
-      if idx != Map::xy_idx(map.width, 40, 25) {
-        map.tiles[idx] = TileType::Wall;
-      }
-    }
-
-    map
   }
 }
